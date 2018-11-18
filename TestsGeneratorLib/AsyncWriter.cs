f0ec7cd59b1866161dc0750f08e91e1e497e6b08
DenisKaminsky <deniskaminsky123@mail.ru>
2018-11-18 16:34:50 +0300

    add async writer

---
+++ TestsGeneratorLib/AsyncWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLib
+{
+    public static class AsyncWriter
+    {
+        public static async Task Write(string destination)
+        {
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+            destination += "\\";//obj
+            using (StreamWriter writer = new StreamWriter(destination))
+            {
+                await writer.WriteAsync();//object
+            }
+        }
+    }
+}
