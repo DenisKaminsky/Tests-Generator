15b5df661e00206d07c8980ef8611a2a13eec8ae
DenisKaminsky <deniskaminsky123@mail.ru>
2018-11-18 15:57:15 +0300

    add dataflow options

--- TestsGeneratorLib/TestsGenerator.cs
+++ TestsGeneratorLib/TestsGenerator.cs
@@ -9,13 +9,30 @@ namespace TestsGeneratorLib
 {
     public class TestsGenerator
     {
-        public TestsGenerator()
-        {
+        private TestsGeneratorConfig _config;
 
+        public TestsGenerator(TestsGeneratorConfig config)
+        {
+            _config = config;
         }
 
         public Task Generate()
         {
+            DataflowLinkOptions linkOptions = new DataflowLinkOptions { PropagateCompletion = true };//цель получает уведомление о завершении/сбое
+            ExecutionDataflowBlockOptions readBlockOptions = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = _config._maxReadTasksCount
+            };
+            ExecutionDataflowBlockOptions processingBlockOptions = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = _config._maxProcessingTasksCount
+            };
+            ExecutionDataflowBlockOptions writeOptions = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = _config._maxWriteTasksCount
+            };
+
+
 
         }
 
