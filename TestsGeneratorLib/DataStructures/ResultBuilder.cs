d6eea8aa5aa5f4d6f2999f850c280c37e2ae5f9a
DenisKaminsky <deniskaminsky123@mail.ru>
2018-11-19 23:41:54 +0300

    get result method

--- TestsGeneratorLib/DataStructures/ResultBuilder.cs
+++ TestsGeneratorLib/DataStructures/ResultBuilder.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace TestsGeneratorLib.DataStructures
 {
     public class ResultBuilder
     {
+        public ResultStructure GetResult(string sourceCode)
+        {
+            SyntaxTree codeTree = CSharpSyntaxTree.ParseText(sourceCode);
+            CompilationUnitSyntax root = codeTree.GetCompilationUnitRoot();
+        }
     }
 }
--- TestsGeneratorLib/DataStructures/ResultStructure.cs
+++ TestsGeneratorLib/DataStructures/ResultStructure.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestsGeneratorLib.ResultStructures
+namespace TestsGeneratorLib.DataStructures
 {
     
     public class ResultStructure
