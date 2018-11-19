2347d9a455db45b54e5d3fa607ec9ee1fa56edbb
DenisKaminsky <deniskaminsky123@mail.ru>
2018-11-20 01:04:10 +0300

    generatedtest class complete

--- TestsGeneratorLib/DataStructures/GeneratedTest.cs
+++ TestsGeneratorLib/DataStructures/GeneratedTest.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestsGeneratorLib.DataStructures
+﻿namespace TestsGeneratorLib.DataStructures
 {
     public class GeneratedTest
     {
         private string _name;
         private string _content;
 
-        
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+        }
+
+        public GeneratedTest(string name,string content)
+        {
+            _name = name;
+            _content = content;
+        }
     }
 }
